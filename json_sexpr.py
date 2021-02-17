27fe6d64a5c7dbcc9cc39df7210c50b757cc8c9c
ihalseide <imitatio-ignotum@protonmail.com>
2021-02-17 14:11:04 -0700

    Improve command line utility
    Yes I know that I "should" use the argparse Python module

--- json_sexpr.py
+++ json_sexpr.py
@@ -9,30 +9,38 @@ def json_to_sexpr (json_str: str) -> str:
     '''Covert JSON syntax to S-Expression syntax'''
     return sexpr.to_s(json.loads(json_str))
 
-def __get_conversion_type (name):
-    first_char = name[0].lower() 
-    if first_char == 'j':
-        # e.g 'JSON' or 'j' or 'js'
-        return sexpr_to_json
-    elif first_char in 'ls': 
-        # e.g 'LISP' or 'SEXPR' or 'l'
-        return json_to_sexpr
-    else:
-        return None
-
-def main ():
-    '''JSON and LISP command-line conversion utility'''
+if __name__ == '__main__':
     import sys
-    if len(sys.argv) > 1:
+
+    def _get_conversion_type (name):
+        if name:
+            first_char = name[0].lower() 
+            if 'j' == first_char:
+                return sexpr_to_json
+            elif 's' == first_char: 
+                return json_to_sexpr
+
+    def _print_usage (): 
+        print('Usage:', sys.argv[0], '[filename]', '-[j|s]')
+    
+    if len(sys.argv) == 3:
         in_file = sys.argv[1]
-        convert_to = sys.argv[2]
-        convert =  __get_conversion_type(convert_to)
+        convert_to = sys.argv[2][1:]
+        convert =  _get_conversion_type(convert_to)
         if convert is None:
-            print('Invalid conversion name; please specify json or lisp', file=sys.stderr)
+            print(sys.argv[0], ': invalid conversion type', file=sys.stderr)
+            sys.exit(-1)
         else:
             in_str = open(in_file).read()
             result = convert(in_str)
             print(result)
-
-if __name__ == '__main__':
-    main()
+    elif len(sys.argv) == 2 and sys.argv[1][:2] == '-h':
+        print('Help for:', sys.argv[0])
+        print('\nThis utility is for converting JSON or S-Expression formatted files into the other format. Output is printed to stdout.\n')
+        _print_usage()
+        print('\t-j to convert S-Expressions to JSON')
+        print('\t-s to convert JSON to S-Expressions')
+    else:
+        _print_usage()
+        sys.exit(-1)
+    sys.exit(0)
